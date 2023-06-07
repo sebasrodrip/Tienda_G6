289b891ec3b893a60c18dcfa935042599e2e00d4
sebasrodrip <106126186+sebasrodrip@users.noreply.github.com>
2023-06-06 20:39:43 -0600

    Primer push proyecto
    Push del proyecto

---
+++ KN_WEB/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tienda_G6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+    }
+}
