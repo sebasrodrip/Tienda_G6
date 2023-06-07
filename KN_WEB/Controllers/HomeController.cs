8135688f9a66506bd763726efc12ac53b5773414
sebasrodrip <106126186+sebasrodrip@users.noreply.github.com>
2023-06-07 14:10:43 -0600

    Adicion de Razor a Login y Registro

--- KN_WEB/Controllers/HomeController.cs
+++ KN_WEB/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using KN_WEB.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Tienda_G6.Controllers
+namespace KN_WEB.Controllers
 {
     public class HomeController : Controller
     {
@@ -14,15 +15,30 @@ namespace Tienda_G6.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(UsuarioEnt entidad)
+        {
+            //PROGRAMACION
+            return RedirectToAction("Index","Home");
         }
 
         [HttpGet]
         public ActionResult Registro()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarUsuario(UsuarioEnt entidad)
+        {
+            //PROGRAMACION
+            return RedirectToAction("Login", "Home");
         }
 
     }
